e5ed8e86fc05790dd77cdd0c6038dd1be7e1db17
Andrew <aknittle623@gmail.com>
2019-03-27 22:04:45 -0400

    Update isoabt_prototype.py
    Using pygame to draw shape. Some copy pasting done.

--- isoabt_prototype.py
+++ isoabt_prototype.py
@@ -1,6 +1,6 @@
 """Prototype of game"""
 
-#import pygame
+import pygame
 
 """Class used to describe points in 2D space"""
 
@@ -106,9 +106,50 @@ class Shape:
         return connected_edges
 
 
+def display_shape(connected_edges):
+    pygame.init()
+
+    # Define the colors we will use in RGB format
+    BLACK = (0, 0, 0)
+    WHITE = (255, 255, 255)
+    BLUE = (0, 0, 255)
+    GREEN = (0, 255, 0)
+    RED = (255, 0, 0)
+
+    # Set the height and width of the screen
+    size = [400, 300]
+    screen = pygame.display.set_mode(size)
+    pygame.display.set_caption("Test 1 2 3")
+
+    # Loop until the user clicks the close button.
+    done = False
+    clock = pygame.time.Clock()
+    while not done:
+
+        # This limits the while loop to a max of 10 times per second.
+        # Leave this out and we will use all CPU we can.
+        clock.tick(10)
+
+        for event in pygame.event.get():  # User did something
+            if event.type == pygame.QUIT:  # If user clicked close
+                done = True  # Flag that we are done so we exit this loop
+
+        # All drawing code happens after the for loop and but
+        # inside the main while done==False loop.
+
+        # Clear the screen and set the screen background
+        screen.fill(WHITE)
+        pygame.draw.lines(screen, BLACK, False, connected_edges[0], 5)
+        # Go ahead and update the screen with what we've drawn.
+        # This MUST happen after all the other drawing commands.
+        pygame.display.flip()
+
+    # Be IDLE friendly
+    pygame.quit()
+
+
 def main():
-    #pygame.init()
-    sample_points = [(1, 1), (2, 0)]
+    sample_points = [(500, 0), (500, 500)]
     sample_shape = Shape()
     point = GeoPoint(0, 0)
     sample_shape.add_point(point)
@@ -116,6 +157,7 @@ def main():
         point = GeoPoint(i[0], i[1])
         sample_shape.add_point(point, True)
     print(sample_shape.draw_shape())
+    display_shape(sample_shape.draw_shape())
 
 
 if __name__ == "__main__":
